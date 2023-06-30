9929a7e6be1cbc449cd8d5a960353eec28a15980
YuanRongtao <chrisyuan1921@gmail.com>
2023-06-30 17:44:38 +0800

    create Project HelloGit commit

---
+++ src/main/java/com/itheima/controller/TestController2.java
@@ -0,0 +1,16 @@
+package com.itheima.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ClassName: TestController2
+ * Package: com.itheima.controller
+ * Description:
+ *
+ * @Author YRTau
+ * @Create 2023/6/30 17:39
+ * @Version 1.0
+ */
+@RestController
+public class TestController2 {
+}
